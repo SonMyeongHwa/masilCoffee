8240eac005a91539ec525eddfda888c1f1224fad
JEONGSUJONG <jeon95ujong@gmail.com>
2023-12-02 01:19:57 +0900

    fix:url

--- client/src/api/interceptor/apiInstance.jsx
+++ client/src/api/interceptor/apiInstance.jsx
@@ -16,7 +16,7 @@ apiInstance.interceptors.request.use(
 );
 
 const apiInstanceForm = axios.create({
-  baseURL: `${process.env.REACT_APP_SERVER_URL}`,
+  baseURL: "http://kdt-sw-6-team03.elicecoding.com",
   headers: { "Content-Type": "multipart/form-data" },
 });
 
@@ -32,7 +32,7 @@ apiInstanceForm.interceptors.request.use(
 );
 
 const apiInstanceNonAuth = axios.create({
-  baseURL: `${process.env.REACT_APP_SERVER_URL}`,
+  baseURL: "http://kdt-sw-6-team03.elicecoding.com",
 });
 
 export { apiInstance, apiInstanceForm, apiInstanceNonAuth };
